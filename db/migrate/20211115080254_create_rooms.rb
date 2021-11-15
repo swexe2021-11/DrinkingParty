001851d4ba38cc8e754c26e491a87d6e752ba688
RikuKamibayashi <1911140050p@ed.fuk.kindai.ac.jp>
2021-11-15 08:04:26 +0000

    initial commit

--- db/migrate/20211115080254_create_rooms.rb
+++ db/migrate/20211115080254_create_rooms.rb
@@ -1,7 +1,7 @@
 class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
-      t.string :title
+      t.integer :title
       t.integer :room_id
       t.inreger :user_id
       t.binary :file
--- db/migrate/20211115080336_create_chats.rb
+++ db/migrate/20211115080336_create_chats.rb
@@ -3,6 +3,7 @@ class CreateChats < ActiveRecord::Migration[5.2]
     create_table :chats do |t|
       t.integer :user_id
       t.integer :room_id
+      t.string :message
 
       t.timestamps
     end
