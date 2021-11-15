4ce7543d3d0aa071ccb625f1438f1b4d2edf1487
Ono Shota <you@example.com>
2021-11-15 08:12:47 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :users
+  resources :tweets
+  get 'top/main'
+  post 'top/login'
+  get 'top/logout'
+  resources :likes
+root 'top#index'end
