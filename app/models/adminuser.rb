23463fa6c2ef26ce5547a525e9ad30754b793045
Ono Shota <you@example.com>
2021-12-06 07:18:02 +0000

    viewファイル修正

--- app/models/adminuser.rb
+++ app/models/adminuser.rb
@@ -1,2 +1,11 @@
 class Adminuser < ApplicationRecord
+        validates :password, presence: true, confirmation: true
+    attr_accessor :password, :password_confirmation
+    
+    def password=(val)
+        if val.present?
+          self.pass2 = BCrypt::Password.create(val)
+        end
+        @password = val
+    end
 end
